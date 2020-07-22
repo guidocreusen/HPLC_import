ff8b90995098f32e2616537b1585df9a322fcfa9
Guido Creusen <guidocreusen@gmail.com>
2020-07-22 22:07:04 +0200

    created main file

---
+++ src/main.py
@@ -0,0 +1,17 @@
+# this script can import multiple HPLC traces, subtract a baseline for each, and then unify them into a single file
+# uses ktinker GUI
+
+class MainController:
+    
+    def __init__(self):
+        
+        print("initializing program")
+        
+        #TODO: create a GUI controller here
+        
+    def start(self):
+        
+        print("starting program")
+        
+program = MainController()
+program.start()

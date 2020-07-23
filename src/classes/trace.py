7f2220baa1b6b27c7e5098dd9284f8c552fbe8a5
Guido Creusen <guidocreusen@gmail.com>
2020-07-23 11:25:22 +0200

    baseline can be loaded into a line list

--- src/classes/trace.py
+++ src/classes/trace.py
@@ -4,6 +4,7 @@ import classes.run
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askopenfilenames
+from tkinter import messagebox # required for messagebox to work, even with tkinter/tk.messagebox as messagebox import isn't part of tkinter __init__
 
 class Trace():
     
@@ -21,6 +22,20 @@ class Trace():
         
         baseline_filepath = askopenfilename(initialdir = "", filetypes = (("Text File", "*.txt"),), title = "Choose a baseline file.")
         
+        print ("\nloading file: " + baseline_filepath)
+        
+        try:
+            baseline_file = open(baseline_filepath, 'r')
+                
+            # load all lines into a list, and remove the newline ends
+            baseline_file_lines = [line.strip('\n') for line in baseline_file.readlines()] # in python3 map use is discouraged and ist comprehension is faster
+                
+            print("File readout:\n" + str(baseline_file_lines))
+            
+        except:
+            
+            return False
+        
     def load_measurements(self):
         
         print("loading measurement files to trace")
@@ -29,6 +44,6 @@ class Trace():
         measurements_filepaths_list = list(measurements_filepaths)
         
         
-        print(measurements_filepaths)
-        print(measurements_filepaths_list)
+        print ("\nloading files: " + measurements_filepaths_list)
+
         
